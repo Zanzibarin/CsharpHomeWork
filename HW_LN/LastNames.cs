90f50de8e816fd2e7212d68404a34d094346d43b
Zanzibarin <alex_potemkin@mail.ru>
2021-11-22 20:55:08 +0300

    Add files via upload

---
+++ HW_LN/LastNames.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LastNames
+{
+    class LastNames
+    {
+        private List<string> lname;
+
+        public LastNames(List<string> initialList)
+        {
+            lname = new List<string>();
+            foreach (string a in initialList)
+            {
+                lname.Add(a);
+            }
+            lname.Sort();
+        }
+
+        private int getLastNameIndex(string searchLN)
+        {
+            int index = -1;
+            for (int i = 0; i < lname.Count; ++i)
+            {
+                if (lname[i].Equals(searchLN))
+                {
+                    index = i + 1;
+                    break;
+                }
+
+            }
+            return index;
+        }
+
+        private void addLastName(string addLN)
+        {
+            lname.Add(addLN);
+            lname.Sort();
+        }
+
+        static string Capitalize(string s)
+        {
+            s = s[0].ToString().ToUpper() + s.Substring(1).ToLower();
+            return s;
+        }
+
+        public string ProcessLastName(string lastName)
+        {
+            lastName = Capitalize(lastName);
+            int result = getLastNameIndex(lastName);
+            if (result < 0)
+            {
+                addLastName(lastName);
+                result = getLastNameIndex(lastName);
+            }
+
+            string ret = $"Querried last name {lastName} has index = {result} \n";
+            ret += string.Join("\n", lname);
+            return ret;
+
+        }
+    }
+}
---
+++ HW_LN/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LastNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> inital = new List<string>() { "Иванов", "Петров", "Сидоров" };
+            LastNames lname = new LastNames(inital);
+            Console.Write("Enter search query: ");
+            Console.WriteLine(lname.ProcessLastName(Console.ReadLine()));
+        }
+    }
+}
